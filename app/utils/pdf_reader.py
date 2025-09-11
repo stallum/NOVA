1012d6758db752f2b244f3eb1d288d5027acfea5
Artur <arturgab008@gmail.com>
2025-09-11 14:32:50 -0300

    feat: implemante pdf reader on program

--- app/utils/pdf_reader.py
+++ app/utils/pdf_reader.py
@@ -2,8 +2,6 @@ from langchain_community.document_loaders import PyMuPDFLoader
 from pathlib import Path
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-from time import sleep
-
 from styling import Style
 
 class PDF():
@@ -19,24 +17,27 @@ class PDF():
             except Exception as e:
                 print(f"ERROR: {e}")
         
-        self.spliter = RecursiveCharacterTextSplitter(chunk_size=300, overlap=30)
-        self.chunk = self.spliter.split_documents(self.docs)
+        self.spliter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=30)
+        self.chunks = self.spliter.split_documents(self.docs)
+        for chunk in self.chunks:
+            print(chunk)
+            print('-'*30)
 
 
 
 if __name__ == "__main__": 
-    print("(NOVA): ", end='')
-    st = Style()
     pdf = PDF()
 
-    st.typeEffect("Qual o diretório onde encontra-se o pdf?\n \n")
+    # st.typeEffect("Qual o diretório onde encontra-se o pdf?\n \n")
             
-    path = input("(YOU): ")
+    # path = input("(YOU): ")
 
-    print("(NOVA): ", end='')
-    st.typeEffect("Qual o nome do pdf que você quer que eu leia?")
+    # print("(NOVA): ", end='')
+    # st.typeEffect("Qual o nome do pdf que você quer que eu leia?")
 
-    arq_name = input("(YOU): ")
+    # arq_name = input("(YOU): ")
+
+    path = '/home/stallum/Downloads'
+    arq_name = 'randomforest2001'
 
     pdf.ler_pdf(path=path, arq=arq_name)
-
