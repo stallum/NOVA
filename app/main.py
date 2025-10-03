2f1761734c38d802c899226a61a475f10130ae23
Artur <stallumgp8@gmail.com>
2025-10-02 21:58:53 -0300

    refactor: organizing the main.py page

--- app/main.py
+++ app/main.py
@@ -20,19 +20,7 @@ if __name__ == "__main__":
     bot = NOVA()
     msg = bot.mostrar_interface()  
 
-    while msg != "/quit":
-
-        if msg == "1":
-            print("\n \nMe envie o link do vídeo do youtube que quer baixar. ou \"/back\" para voltar ao menu\n")
-
-            link_yt= input("\n(You): ")
-
-            bot.yt.baixarVideo(link_yt)
-            sleep(2)
-            msg = None
-        
-        if msg == "2":
-            pass
+    while msg != "/quit":        
 
         msg = bot.mostrar_interface()
     
@@ -43,13 +31,3 @@ if __name__ == "__main__":
         sleep(2.4)
         bot.limpar_tela()
         sys.exit()
-
-
-        # st.typeEffect("Qual o diretório onde encontra-se o pdf?\n \n")
-            
-        # path = input("(YOU): ")
-
-        # print("(NOVA): ", end='')
-        # st.typeEffect("Qual o nome do pdf que você quer que eu leia?")
-
-        # arq_name = input("(YOU): ")
